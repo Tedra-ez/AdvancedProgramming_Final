58e6317efcca7ab79d11e11d59eda7f04f969dc6
Greatjobson <zumabejba@gmail.com>
2026-02-01 22:56:17 +0500

    Add MongoDB client implementation with connection and collection methods

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,17 @@
 package config
+
+import "os"
+
+type Config struct {
+	MongoURI string
+	Port     string
+}
+
+func Load() *Config {
+	uri := os.Getenv("MONGODB_URI")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	return &Config{MongoURI: uri, Port: port}
+}
--- internal/db/mongo.go
+++ internal/db/mongo.go
@@ -1,1 +1,43 @@
 package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoDBClient struct {
+	client   *mongo.Client
+	database string
+}
+
+const defaultDatabase = "clothes_store"
+
+func NewMongoDBClient(ctx context.Context, uri string) (*MongoDBClient, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, err
+	}
+	log.Println("MongoDB connected")
+	return &MongoDBClient{client: client, database: defaultDatabase}, nil
+}
+
+func (c *MongoDBClient) Database() *mongo.Database {
+	return c.client.Database(c.database)
+}
+
+func (c *MongoDBClient) Collection(name string) *mongo.Collection {
+	return c.Database().Collection(name)
+}
+
+func (c *MongoDBClient) Close(ctx context.Context) error {
+	return c.client.Disconnect(ctx)
+}
